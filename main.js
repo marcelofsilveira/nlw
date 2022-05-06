e52d98a4be92e1576788ea51e425b1523074551d
Marcelo <silveira.marcelo2010@gmail.com>
2022-05-06 14:11:14 -0300

    corrigindo erro de merge

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 /* abre e fecha o menu quando clicar no ícone hamburguer e X */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
@@ -30,67 +29,4 @@ window.addEventListener('scroll', function (){
     header.classList.remove('scroll')
   }
 })
-=======
-/* abre e fecha o menu quando clicar no ícone hamburguer e X */
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
 
-for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
-}
-
-/* quando clicar em um item do menu, esconder o menu */
-const links = document.querySelectorAll('nav ul li a')
-
-for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
-}
-
-/* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function (){
-  if (this.window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-})
->>>>>>> 93c978519f64605cb4de965239534cd883d062d5
-=======
-/* abre e fecha o menu quando clicar no ícone hamburguer e X */
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
-
-for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
-}
-
-/* quando clicar em um item do menu, esconder o menu */
-const links = document.querySelectorAll('nav ul li a')
-
-for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
-}
-
-/* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function (){
-  if (this.window.scrollY >= navHeight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-})
->>>>>>> 93c978519f64605cb4de965239534cd883d062d5
